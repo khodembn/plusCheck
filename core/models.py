3990da929beb8ef1149e730ea57f2d095a55f790
khodembn <mbmr2089@gmail.com>
2026-08-22 18:33:09 +0330

    Create MoodEntry model and migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class MoodEntry(models.Model):
+    score = models.IntegerField()
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField()
+    energy_level = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.score} - {self.created_at}"
